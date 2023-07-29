464d2155534cfb3b43c661af02ebbdeb8015cd54
Javier Fernández <javier.fernandez@novanta.com>
2023-07-29 10:30:14 +0200

    increase slightly mbx tread timeout

--- soem/ethercatmain.c
+++ soem/ethercatmain.c
@@ -1035,7 +1035,7 @@ int ecx_mbxreceive(ecx_contextt *context, uint16 slave, ec_mbxbuft *mbx, int tim
          mbxh = (ec_mbxheadert *)mbx;
          do
          {
-            wkc = ecx_FPRD(context->port, configadr, mbxro, mbxl, mbx, EC_TIMEOUTRET); /* get mailbox */
+            wkc = ecx_FPRD(context->port, configadr, mbxro, mbxl, mbx, EC_TIMEOUTRET3); /* get mailbox */
             if ((wkc > 0) && ((mbxh->mbxtype & 0x0f) == 0x00)) /* Mailbox error response? */
             {
                MBXEp = (ec_mbxerrort *)mbx;
